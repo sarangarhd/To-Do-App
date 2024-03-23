2d70fcd1e6322903c41afd36c8d72eaa03c7194e
sarangarhd <rhdsaranga@gmail.com>
2024-03-24 01:08:40 +0530

    Building the session factory

--- src/com/devstack/app/db/HibernateUtill.java
+++ src/com/devstack/app/db/HibernateUtill.java
@@ -1,4 +1,36 @@
 package com.devstack.app.db;
 
+import com.devstack.app.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateUtill {
+    private static SessionFactory sessionFactory=buildSessionFactory();
+    private HibernateUtill(){}
+
+    private static SessionFactory buildSessionFactory(){
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure( "hibernate.cfg.xml" )
+                .build();
+
+        Metadata metadata = new MetadataSources( standardRegistry )
+                .addAnnotatedClass( User.class )
+
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy( ImplicitNamingStrategyJpaCompliantImpl.INSTANCE )
+                .build();
+
+       return metadata.getSessionFactoryBuilder()
+
+                .build();
+    }
+
+    public static Session openSession(){
+        return sessionFactory.openSession();
+    }
 }
